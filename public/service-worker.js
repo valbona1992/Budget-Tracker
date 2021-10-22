7834b1c1877fa34c3ff841b889358363464c3cfe
valbona1992 <valbona12@gmail.com>
2021-10-22 17:26:41 -0500

    update file paths on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,11 @@
 const FILES_TO_CACHE = [
-   
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/db.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
