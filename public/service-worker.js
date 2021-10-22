0ad5afdf2ecf8bcd5e356b900a9c54fc88db6370
valbona1992 <valbona12@gmail.com>
2021-10-22 18:26:29 -0500

    add file path to service worker for manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/styles.css",
     "/index.js",
+    "/manifest.webmanifest",
     "/db.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
